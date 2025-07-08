47898b8665ea768866c6924e7e899f04482e5b4f
Justin Bright <thefastlane@gmail.com>
2025-07-08 16:15:10 -0400

    port 4000

--- src/server.ts
+++ src/server.ts
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4001;
+  const port = process.env['PORT'] || 4000;
   app.listen(port, (error) => {
     if (error) {
       throw error;
