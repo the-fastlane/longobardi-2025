cf156030a845eda7e7af6714274e7492c2d96161
Justin Bright <thefastlane@gmail.com>
2025-07-09 07:47:11 -0400

    parse json

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { join } from 'node:path';
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
+app.use(express.json()); // Add this line to parse JSON bodies
 const angularApp = new AngularNodeAppEngine();
 
 /**
