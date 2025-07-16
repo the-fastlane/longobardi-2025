73fe2fffda9ee921f989a4d751571dac78aac488
Justin Bright <thefastlane@gmail.com>
2025-07-16 15:54:15 -0400

    comcast email

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,7 @@ app.post('/api/send-lead', async (req, res) => {
 
     await transporter.sendMail({
       from: '"Joe Mortgage Pro" <leads@joemortgagepro.com>',
-      to: 'jlongobardi@vandykmortgage.com',
+      to: 'jlongo527@comcast.net',
       subject: `${formData.loanType} - Lead Received`,
       replyTo: 'no-reply@joemortgagepro.com',
       text: `A new mortgage lead has been submitted.
